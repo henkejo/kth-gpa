b7916f08332b1f990e39fe68f382bb96757b3f0c
jolerus <henrik.jolerus@gmail.com>
2020-11-20 04:27:24 +0100

    Tweak styles and add disable for N/A courses

--- contentScript.js
+++ contentScript.js
@@ -6,8 +6,39 @@ document.arrive("ladok-avslutad-kurs .card-body", function() {
     const courseStrings = this.querySelector(".ldk-visa-desktop > a").innerText.split("|");
     const courseCode = courseStrings[2].split(" ")[1];
     const credits = Number(courseStrings[1].split(" ")[1]);
-
-    console.log(credits);
+    
+    const grade = this.querySelector(".card-body  strong").innerText.split(" ")[1];
+    let applicable = true;
+    let gradeScore;
+    switch (grade) {
+        case "-":
+            applicable = false;
+            break;
+        case "P":
+            applicable = false;
+            break;
+        case "F":
+            gradeScore = 0;
+            break;
+        case "E":
+            gradeScore = 3;
+            break;
+        case "D":
+            gradeScore = 3.5;
+            break;
+        case "C":
+            gradeScore = 4;
+            break;
+        case "B":
+            gradeScore = 4.5;
+            break;
+        case "A":
+            gradeScore = 5;
+            break;
+        default:
+            break;
+    }
+    console.log(gradeScore);
 
     // This beautiful table holds:
     // (1) completed course info on the top
@@ -31,6 +62,7 @@ document.arrive("ladok-avslutad-kurs .card-body", function() {
     //  --- "Include in GPA count:"-checkbox
     let checkBox = document.createElement("div");
     checkBox.classList.add("checkBox");
+    checkBox.style.display = "inline";
     bottomCellGpa.appendChild(checkBox);
     // Checkbox label
     let label = document.createElement("label");
@@ -39,7 +71,20 @@ document.arrive("ladok-avslutad-kurs .card-body", function() {
     // Actual checkbox
     let box = document.createElement("input");
     box.type = "checkbox";
-    box.checked = true;
+    if (applicable) {
+        box.checked = true;
+        let p = document.createElement("p")
+        p.innerText = "(Not applicable for GPA count)";
+        p.style.display = "inline";
+    } else {
+        box.checked = false;
+        box.disabled = true; 
+        label.style.color = "grey";
+        let p = document.createElement("p")
+        p.innerText = "(Not applicable for GPA count)";
+        p.style.display = "inline";
+        checkBox.appendChild(p);
+    }
     label.appendChild(box);
     this.appendChild(table);
 });
