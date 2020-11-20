24f89b1cfd33259d0101b6cbf3bb29ba1d1e41e9
jolerus <henrik.jolerus@gmail.com>
2020-11-20 02:18:02 +0100

    Add grid & course info cell for each completed course

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,21 @@
 console.log('hello world');
 
 document.arrive("ladok-avslutad-kurs .card-body", function() {
-    // 'this' refers to the newly created element
-    let p = document.createElement("p");
-    p.innerText = "Test";
-    this.appendChild(p);
+    // This beautiful CSS grids table holds:
+    // (1) completed course info on the left
+    // (2) and KTHgpa stuff on the right
+    let table = document.createElement("div");
+    table.classList.add("grid-container");
+
+    let leftCell = document.createElement("div");
+    leftCell.classList.add("leftCell");
+    table.appendChild(leftCell);
+
+    // Transferring (1) into the table
+    while (this.childNodes.length > 0) {
+        leftCell.appendChild(this.firstChild);
+    }
+    this.appendChild(table);
+
     console.log('ladok-avslutad-kurs');
 });
